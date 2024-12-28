d165fd651572d25252ef2a04d4f19a444b7b0a79
dicutrim <diegocutrimcontato@gmail.com>
2024-12-28 14:20:29 -0300

    Criação do projeto devstore_backend

---
+++ src/main/java/com/portfolio/devstore_backend/DevstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.devstore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstoreBackendApplication.class, args);
+	}
+
+}
